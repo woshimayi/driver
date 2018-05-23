0151440ba1c35d4ef0d8624f77b394d9489ed439
woshidamayi <git@github.com:woshimayi/openwrt.git>
2018-05-24 00:26:12 +0800

    OverView: module universal
    BUG:
    
    Description:

--- module_read/char_dev.c
+++ module_read/char_dev.c
@@ -27,6 +27,35 @@ struct globalmem_dev {
 
 struct globalmem_dev *globalmem_devp;
 
+static int globalmem_open(struct inode *inode, struct file *filp)
+{
+	filp->private_data = globalmem_devp;
+	return 0;
+}
+
+static int globalmem_release(struct inode *inode, struct file *filp)
+{
+	return 0;
+}
+
+static long globalmem_ioctl(struct file *filp, unsigned int cmd,
+			    unsigned long arg)
+{
+	struct globalmem_dev *dev = filp->private_data;
+
+	switch (cmd) {
+	case MEM_CLEAR:
+		memset(dev->mem, 0, GLOBALMEM_SIZE);
+		printk(KERN_INFO "globalmem is set to zero\n");
+		break;
+
+	default:
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
 static ssize_t globalmem_read(struct file *filp, char __user * buf, size_t size,
 			      loff_t * ppos)
 {
@@ -52,12 +81,86 @@ static ssize_t globalmem_read(struct file *filp, char __user * buf, size_t size,
 	return ret;
 }
 
+static ssize_t globalmem_write(struct file *filp, const char __user * buf,
+			       size_t size, loff_t * ppos)
+{
+	unsigned long p = *ppos;
+	unsigned int count = size;
+	int ret = 0;
+	struct globalmem_dev *dev = filp->private_data;
+
+	if (p >= GLOBALMEM_SIZE)
+		return 0;
+	if (count > GLOBALMEM_SIZE - p)
+		count = GLOBALMEM_SIZE - p;
+
+	if (copy_from_user(dev->mem + p, buf, count))
+		ret = -EFAULT;
+	else {
+		*ppos += count;
+		ret = count;
+
+		printk(KERN_INFO "written %u bytes(s) from %lu\n", count, p);
+	}
+
+	return ret;
+}
+
+static loff_t globalmem_llseek(struct file *filp, loff_t offset, int orig)
+{
+	loff_t ret = 0;
+	switch (orig) {
+	case 0:
+		if (offset < 0) {
+			ret = -EINVAL;
+			break;
+		}
+		if ((unsigned int)offset > GLOBALMEM_SIZE) {
+			ret = -EINVAL;
+			break;
+		}
+		filp->f_pos = (unsigned int)offset;
+		ret = filp->f_pos;
+		break;
+	case 1:
+		if ((filp->f_pos + offset) > GLOBALMEM_SIZE) {
+			ret = -EINVAL;
+			break;
+		}
+		if ((filp->f_pos + offset) < 0) {
+			ret = -EINVAL;
+			break;
+		}
+		filp->f_pos += offset;
+		ret = filp->f_pos;
+		break;
+	default:
+		ret = -EINVAL;
+		break;
+	}
+	return ret;
+}
 
 static const struct file_operations globalmem_fops = {
 	.owner = THIS_MODULE,
+	.llseek = globalmem_llseek,
 	.read = globalmem_read,
+	.write = globalmem_write,
+	.unlocked_ioctl = globalmem_ioctl,
+	.open = globalmem_open,
+	.release = globalmem_release,
 };
 
+static void globalmem_setup_cdev(struct globalmem_dev *dev, int index)
+{
+	int err, devno = MKDEV(globalmem_major, index);
+
+	cdev_init(&dev->cdev, &globalmem_fops);
+	dev->cdev.owner = THIS_MODULE;
+	err = cdev_add(&dev->cdev, devno, 1);
+	if (err)
+		printk(KERN_NOTICE "Error %d adding globalmem%d", err, index);
+}
 
 static int __init globalmem_init(void)
 {
@@ -65,12 +168,9 @@ static int __init globalmem_init(void)
 	dev_t devno = MKDEV(globalmem_major, 0);
 
 	if (globalmem_major)
-	{
-		ret = register_chrdev_region(devno, 1, "globalmem_zs");
-	}
-	else 
-	{
-		ret = alloc_chrdev_region(&devno, 0, 1, "globalmem_zx");
+		ret = register_chrdev_region(devno, 1, "globalmem");
+	else {
+		ret = alloc_chrdev_region(&devno, 0, 1, "globalmem");
 		globalmem_major = MAJOR(devno);
 	}
 	if (ret < 0)
@@ -82,6 +182,7 @@ static int __init globalmem_init(void)
 		goto fail_malloc;
 	}
 
+	globalmem_setup_cdev(globalmem_devp, 0);
 	return 0;
 
  fail_malloc:
@@ -98,4 +199,5 @@ static void __exit globalmem_exit(void)
 }
 module_exit(globalmem_exit);
 
+MODULE_AUTHOR("Barry Song <baohua@kernel.org>");
 MODULE_LICENSE("GPL v2");
