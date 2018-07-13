df06df030621605eb8a4ee94edf5b472b57eda89
woshidamayi <woshidamayi@Gmail.com>
2018-07-13 13:39:10 +0800

    问题描述：添加timeout函数
    解决方法：添加timeout函数

--- user/call/timeout.c
+++ user/call/timeout.c
@@ -222,9 +222,45 @@ int cmsLck_acquireLockWithTimeoutTraced(const char* callerFuncName, int timeoutM
 
 
 
-int main()
+int timeout(int timeout, char sec)
 {
+	long long tmpTime = 0;
+	if (timeout == 0)
+	{
+		return 0;
+	}
 
+
+	switch(sec)
+	{
+		case 'u':
+			tmpTime = timeout;
+			break;
+		case 'm':
+			tmpTime = timeout*1000;
+			break;
+		default:
+			tmpTime = timeout*1000000;
+	}
+
+	float time_use=0;
+	struct timeval start;
+	struct timeval end;//struct timezone tz;
+	gettimeofday(&start,NULL); //gettimeofday(&start,&tz);
+	while(1)
+	{
+		gettimeofday(&end,NULL);
+		time_use=(end.tv_sec-start.tv_sec)*1000000+(end.tv_usec-start.tv_usec);//Î¢Ãë
+ 		printf("time_use = %f\n", time_use);
+		if(time_use>=tmpTime)
+		{
+			printf("time is enough!\n");
+			return 1;
+		}
+		else
+		{
+//			break;
+		}
+	}
 	return 0;
 }
-
