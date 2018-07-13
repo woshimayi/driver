f402ef59dc77982cae4a705abcdc2ed9f85cfb03
woshidamayi <woshidamayi@Gmail.com>
2018-07-13 11:00:40 +0800

    问题描述：添加user程序
    解决方法：添加user程序

---
+++ user/call/main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "call_function.h"
+#include "vos_log.h"
+
+#include <fcntl.h>
+#include <getopt.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <errno.h>
+#include <unistd.h>
+
+int main(int argc, char * argv[])
+{
+
+   return 0;
+}
