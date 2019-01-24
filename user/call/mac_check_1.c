fb16ba64df47a3a8c6e510c5f37a131774445327
woshidamayi <woshidamayi@Gmail.com>
2019-01-24 20:01:05 +0800

    问题描述：call
    解决方法：

--- user/call/mac_check_1.c
+++ user/call/mac_check_1.c
@@ -5,43 +5,43 @@
 #include <string.h>
 #include <ctype.h>
 
- 
- 
+
+#ifdef linux
 int is_valid_mac_addr(char* mac)
-{ 
+{
 	int status = 0;
 	const char * pattern = "^([A-Fa-f0-9]{2}[-,:]){5}[A-Fa-f0-9]{2}$";
-	const int cflags = REG_EXTENDED | REG_NEWLINE;	
-	
+	const int cflags = REG_EXTENDED | REG_NEWLINE;
+
 	char ebuf[128] = {0};
 	regmatch_t pmatch[1] = {{0}};
 	int nmatch = 10;
 	regex_t reg;
- 
- 
+
+
 	status = regcomp(&reg, pattern, cflags);//编译正则模式
 	if(status != 0) {
 		regerror(status, &reg, ebuf, sizeof(ebuf));
 		printf("%s:%d regcomp fail: %s , pattern '%s' \n", __FUNCTION__, __LINE__, ebuf, pattern);
 		goto failed;
 	}
- 
+
 	status = regexec(&reg, mac, nmatch, pmatch,0);//执行正则表达式和缓存的比较,
 	if(status != 0) {
 		regerror(status, &reg, ebuf, sizeof(ebuf));
 		printf("%s:%d regexec fail: %s , mac:\"%s\" \n", __FUNCTION__, __LINE__, ebuf, mac);
 		goto failed;
 	}
- 
+
 	printf("[%s]:%d match success.\n", __FUNCTION__, __LINE__);
 	regfree(&reg);
 	return 0;
- 
+
 failed:
 	regfree(&reg);
 	return -1;
 }
-
+#endif // linux
 
 
 int is_valid_mac(const char *mac)
@@ -52,27 +52,27 @@ int is_valid_mac(const char *mac)
     int colon_count = 0;
     const char *start = mac;
     const char *end = mac + len - 1;
- 
+
     if (mac == NULL || strlen(mac) < 17)
     {
         return -1;
     }
- 
+
     while (end > start && isspace(*end))
     {
         --end;
     }
- 
+
     while (start < end && isspace(*start))
     {
         ++start;
     }
- 
+
     if ((end - start) != 16 || isxdigit(*start) == 0 || isxdigit(*end) == 0)
     {
         return -1;
     }
- 
+
     while (start <= end)
     {
         while (start <= end && isxdigit(*start))
@@ -84,7 +84,7 @@ int is_valid_mac(const char *mac)
                 break;
             }
         }
- 
+
         if (start <= end )
         {
             if (*start == ':' && num_count == 2)
@@ -104,18 +104,11 @@ int is_valid_mac(const char *mac)
             break;
         }
     }
- 
+
     if (ret == 0 && colon_count != 5)
     {
         ret = -1;
     }
- 
+
     return ret;
-}
-
-
-int main(int argc, char *argv[])
-{
-	printf("%d\n", is_valid_mac(argv[1]));
-	return 0;
 }
---
+++ user/call/regcomp.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int isValidMac(char *value) 
+{
+ int r; //r=0:valid, else not valid
+  char *reg="^[0-9A-F]\\([0-9A-F]\\:[0-9A-F]\\)\\{5\\}[0-9A-F]$";
+  r=ereg(reg,value);
+  return r;
+
+}
+
+
+int main()
+{
+
+	return 0;
+}
+
+
