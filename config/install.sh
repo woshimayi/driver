e696727fecf6ae1a2ab7b55321112789b5628842
woshidamayi <woshidamayi@Gmail.com>
2019-11-01 20:29:45 +0800

    问题描述：install config
    解决方法：

--- config/install.sh
+++ config/install.sh
@@ -1,6 +1,6 @@
-sudo apt install vim nmap axel samba samba-dev openssh_server lrzsz  git
+#sudo apt install vim nmap axel samba samba-dev openssh_server lrzsz  git
 
-ssh-keygen -t rsa -b 4096 -C "woshidamayi@Gmail.com"
+#ssh-keygen -t rsa -b 4096 -C "woshidamayi@Gmail.com"
 git config --global user.email "woshidamayi@Gmail.com"
 git config --global user.name "woshidamayi"
 git config --global core.editor vim
