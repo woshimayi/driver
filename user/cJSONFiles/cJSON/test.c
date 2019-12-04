e328db44fe346f3b41d513999a5b01f40fedcfb8
woshidamayi <woshidamayi@Gmail.com>
2019-12-04 17:39:58 +0800

    问题描述：cjson 库文件
    解决方法：

---
+++ user/cJSONFiles/cJSON/test.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+//	char str[1024] = "Some JSON:
+//	\{
+//	    \"name\": "Jack (\"Bee\") Nimble\", 
+//	    \"format\": {
+//	        \"type\":       \"rect\", 
+//	        \"width\":      1920, 
+//	        \"height\":     1080, 
+//	        \"interlace\":  false, 
+//	        \"frame rate\": 24
+//	    \}
+//	\}";
+	
+	char pv[128]  = "InternetGatewayDevice.DeviceInfo.ProvisioningCode";
+	
+//	pv = &pv[15];
+	printf("pv = %s\n", &pv[15]);
+	
+
+	return 0;
+}
+
+
