04391736b72d6fb053702e92b3f3172bbc64f33c
woshidamayi <woshidamayi@Gmail.com>
2019-12-12 20:57:01 +0800

    问题描述：修改astyle 格式化脚本
    解决方法：

--- tools/astyle.sh
+++ tools/astyle.sh
@@ -1,2 +1,5 @@
-astyle --style=allman -k3 -W1 -xG -S -s4 -xb -U -p -xf -xh -xC120 -xL -H -Y -xW -w -n $1
+for f in $(find $1 -name  '*.c' -or -name '*.cpp' -or -name '*.h' -type f)
+do
+astyle --style=allman -k3 -W1 -xG -S -s4 -xb -U -p -xf -xh -xC120 -xL -H -Y -xW -w -n $f
+done
 
