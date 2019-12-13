9ac2c47059a3215ccee7d91e97779e258bafd325
woshidamayi <woshidamayi@Gmail.com>
2019-12-13 13:39:13 +0800

    问题描述：stand driver
    解决方法：

--- config/astyle.sh
+++
@@ -1,2 +0,0 @@
-astyle --style=allman -k3 -W1 -xG -S -s4 -xb -U -p -xf -xh -xC120 -xL -H -Y -xW -w -n $1
-
--- config/install.sh
+++
@@ -1,8 +0,0 @@
-#sudo apt install vim nmap axel samba samba-dev openssh_server lrzsz  git
-
-#ssh-keygen -t rsa -b 4096 -C "woshidamayi@Gmail.com"
-git config --global user.email "woshidamayi@Gmail.com"
-git config --global user.name "woshidamayi"
-git config --global core.editor vim
-git config --global merge.tool vimdiff
-git config --global commit.template ../
