6d31dc44bfee61bdec53aefc9f5887f32cf00450
woshidamayi <woshidamayi@Gmail.com>
2019-12-27 17:33:41 +0800

    问题描述: 网络联通状况监测
    解决方法：

---
+++ user/C/dns_networking.c
@@ -0,0 +1,60 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+int lookup(char *host, int *d)
+{
+    struct addrinfo hints;
+    struct addrinfo *res, *cur;
+    int ret;
+    struct sockaddr_in *addr;
+    char ipbuf[16];
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET; /* Allow IPv4 */
+    hints.ai_flags = AI_PASSIVE; /* For wildcard IP address */
+    hints.ai_protocol = 0; /* Any protocol */
+    hints.ai_socktype = SOCK_STREAM;
+
+    ret = getaddrinfo(host, NULL, &hints, &res);
+
+    if (ret == -1)
+    {
+        perror("getaddrinfo");
+        return ret;
+    }
+
+    for (cur = res; cur != NULL; cur = cur->ai_next)
+    {
+        addr = (struct sockaddr_in *)cur->ai_addr;
+        //printf("%s\n", 
+        inet_ntop(AF_INET, &addr->sin_addr, ipbuf, 16);
+    }
+    freeaddrinfo(res);
+    *d = inet_addr(ipbuf);
+    return ret;
+}
+
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        argv[1] = strdup("www.baidu.com");
+    }
+
+    int d = 0;
+    if (0 != lookup(argv[1], &d))
+    {
+        printf("N\n");
+    }
+    else
+    {
+        printf("%d\n", d);
+        printf("Y\n");
+    }
+}
