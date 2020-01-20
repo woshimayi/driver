aa2a620c58b4716456c30d73ebdff6dac25bce88
woshidamayi <woshidamayi@Gmail.com>
2020-01-20 14:05:36 +0800

    问题描述：start compile scripts 解决方法：

--- uboot/mx6ull_alientek_nand.sh
+++ uboot/mx6ull_alientek_nand.sh
@@ -1,3 +1,3 @@
-RCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6ull_14x14_ddr256_nand_defconfig
 make V=1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j4
