c8cb406c1f9c5e6c420c0fb9ccb35d1dcf77fa3c
woshidamayi <woshidamayi@Gmail.com>
2020-01-20 19:20:06 +0800

    问题描述：uboot change
    解决方法：

--- uboot/mx6ull_alientek_nand.sh
+++ uboot/mx6ull_alientek_nand.sh
@@ -1,3 +1,3 @@
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6ull_14x14_ddr256_nand_defconfig
-make V=1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j4
+make distclean
+make mx6ull_14x14_ddr256_nand_defconfig
+make V=1 -j4
