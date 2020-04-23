a9a9e2c26cb1167ffaeb46e2c688c091a7050003
woshidamayi <woshidamayi@Gmail.com>
2020-04-23 22:42:14 +0800

    问题描述：sscanf
    解决方法：

---
+++ user/C/sscanf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*
+* @param: 
+* @param: 
+* @param: 
+*/
+int main(int argc, char *argv[])
+{
+	char str[128] = "sdfsd_yes_no=2&asda_flag=3";
+	int yes = 0;
+	int flag = 0;
+
+	sscanf(str, "sdfsd_yes_no=%d&asda_flag=%d", &yes, &flag);
+	printf("yes=%d falg=%d\n", yes, flag);
+	return 0;
+}
