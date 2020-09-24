69d8ad6520aa18bdf2e39d269959564b1ffe5f98
woshidamayi <woshidamayi@Gmail.com>
2020-09-24 19:36:50 +0800

    问题描述：添加代码
    解决方法：

---
+++ user/C/fseek-.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define PP printf("date = %s", date);
+
+int main(int atgc, char *argv[])
+{
+	FILE *fd = NULL;
+	char date[63] = {0};
+	
+	fd = fopen("one.mov", "rb");
+	if (NULL == fd)
+	{
+		printf("fail open file"); 
+	}
+	fseek(fd, -10, 2);
+	fread(date, 1, 63, fd);
+	PP
+	fclose(fd);
+	
+	return  0;
+}
+
---
+++ user/C/read-ini/ini.h
@@ -0,0 +1,12 @@
+/*ini.h*/
+#ifndef INI_H
+#define INI_H
+
+#include <stdio.h>  
+#include <string.h>
+
+int GetIniKeyString(char *title,char *key,char *filename,char *buf);
+
+int PutIniKeyString(char *title,char *key,char *val,char *filename);
+
+#endif /*INI_H*/
