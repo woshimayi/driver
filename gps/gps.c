20db4540be376a02a44e9bce2a2d9ac44e227a83
woshidamayi <woshidamayi@Gmail.com>
2020-09-27 17:10:37 +0800

    问题描述：gps
    解决方法：

---
+++ gps/gps.c
@@ -0,0 +1,541 @@
+//
+// Created by ihz on 2020/6/4.
+//
+#include "gps.h"
+
+// 数据分割，可以分割两个连续的分隔符
+static char* strsplit(char** stringp, const char* delim)
+{
+    char* start = *stringp;
+    char* p;
+
+    p = (start != NULL) ? strpbrk(start, delim) : NULL;
+
+    if (p == NULL)
+    {
+        *stringp = NULL;
+    }
+    else
+    {
+        *p = '\0';
+        *stringp = p + 1;
+    }
+
+    return start;
+}
+
+// 统计字符串在另一个字符串中出现的次数
+static int strstr_cnt(char *str, char *substr)
+{
+    char *srcStr = str;
+    int count = 0;
+
+    do
+    {
+        srcStr = strstr(srcStr, substr);
+        if(srcStr != NULL)
+        {
+            count++;
+            srcStr = srcStr + strlen(substr);
+        }
+        else
+        {
+            break;
+        }
+    }while (*srcStr != '\0');
+
+    return count;
+}
+
+#if ENABLE_GGA
+// GGA数据解析
+static GGA gga_data_parse(char *gga_data)
+{
+    GGA gga;
+    unsigned char times = 0;
+    char *p;
+    char *end;
+    char *s = strdup(gga_data);
+
+    p = strsplit(&s, ",");
+    while (p)
+    {
+        switch (times)
+        {
+            case 1:   // UTC
+                strcpy(gga.utc, p);
+                break;
+            case 2:   // lat
+                gga.lat = strtod(p, NULL);
+                break;
+            case 3:   // lat dir
+                gga.lat_dir = p[0];
+                break;
+            case 4:   // lon
+                gga.lon = strtod(p, NULL);
+                break;
+            case 5:   // lon dir
+                gga.lon_dir = p[0];
+                break;
+            case 6:   // quality
+                gga.quality = (unsigned char)strtol(p, NULL, 10);
+                break;
+            case 7:   // sats
+                gga.sats = (unsigned char)strtol(p, NULL, 10);
+                break;
+            case 8:   // hdop
+                gga.hdop = (unsigned char)strtol(p, NULL, 10);
+                break;
+            case 9:   // alt
+                gga.alt = strtof(p, NULL);
+                break;
+            case 11:  // undulation
+                gga.undulation = strtof(p, NULL);
+                break;
+            case 13:  // age
+                gga.age = (unsigned char)strtol(p, NULL, 10);
+                break;
+            case 14:  // stn_ID
+                end = (char *)malloc(sizeof(p));
+                strncpy(end, p, strlen(p)-3);
+                end[strlen(p)-3] = '\0';
+                gga.stn_ID = (unsigned short )strtol(end, NULL, 10);
+                free(end);
+                break;
+            default:
+                break;
+        }
+        p = strsplit(&s, ",");
+        times++;
+    }
+    free(s);
+    return gga;
+}
+#endif
+
+#if ENABLE_GLL
+// GLL数据解析
+static GLL gll_data_parse(char *gll_data)
+{
+    GLL gll;
+    unsigned char times = 0;
+    char *p;
+    char *s = strdup(gll_data);
+
+    p = strsplit(&s, ",");
+    while (p)
+    {
+        switch (times)
+        {
+            case 1:   // lat
+                gll.lat = strtod(p, NULL);
+                break;
+            case 2:   // lat dir
+                gll.lat_dir = p[0];
+                break;
+            case 3:   // lon
+                gll.lon = strtod(p, NULL);
+                break;
+            case 4:   // lon dir
+                gll.lon_dir = p[0];
+                break;
+            case 5:   // lon dir
+                strcpy(gll.utc, p);
+                break;
+            case 6:  // data status
+                gll.data_status = p[0];
+                break;
+            default:
+                break;
+        }
+        p = strsplit(&s, ",");
+        times++;
+    }
+    free(s);
+    return gll;
+}
+#endif
+
+#if ENABLE_GSA
+// 得到GSA数据中的信道信息
+static GSA_PRN *get_prn_data(char *gps_data)
+{
+    GSA_PRN *gsa_prn;
+    unsigned char times = 0;
+    unsigned char i;
+    unsigned char sentences_index = 0;  // 累计找到gsa字段的个数
+    char *p;
+    char *s;
+    char *sentences;
+    int gsa_count;
+
+    // 统计GSA字段的个数
+    gsa_count = strstr_cnt(gps_data, PRE_GSA);
+
+    gsa_prn = (GSA_PRN *)malloc(sizeof(GSA_PRN) * (gsa_count * 12 + 1));
+    memset(gsa_prn, 0, sizeof(GSA_PRN) * (gsa_count * 12 + 1));
+    sentences = strtok(gps_data, "\r\n");
+    while (sentences)
+    {
+        if (strstr(sentences, PRE_GSA))
+        {
+            sentences_index++;
+            s = strdup(sentences);
+            p = strsplit(&s, ",");
+            while (p)
+            {
+                if (times == 2)
+                {
+                    for (i=0; i<12; i++)
+                    {
+                        p = strsplit(&s, ",");
+                        (gsa_prn+i+(sentences_index-1)*12)->total = (unsigned char)gsa_count * 12;
+                        (gsa_prn+i+(sentences_index-1)*12)->prn_ID = i + (sentences_index - 1) * 12;
+                        (gsa_prn+i+(sentences_index-1)*12)->prn = (unsigned char)strtol(p, NULL, 10);
+                    }
+                }
+                p = strsplit(&s, ",");
+                times++;
+            }
+            times = 0;
+        }
+        sentences = strtok(NULL, "\r\n");
+    }
+    free(s);
+    return gsa_prn;
+}
+
+// GSA数据解析
+static GSA gsa_data_parse(char *gsa_data, char *gpsdata)
+{
+    GSA gsa;
+    unsigned char times = 0;
+    char *p;
+    char *end;
+    char *s = strdup(gsa_data);
+    char *alldata = strdup(gpsdata);
+
+    p = strsplit(&s, ",");
+    while (p)
+    {
+        switch (times)
+        {
+            case 1:   // mode_MA
+                gsa.mode_MA = p[0];
+                break;
+            case 2:   // mode_123
+                gsa.mode_123 = p[0];
+                break;
+            case 3:   // prn
+                gsa.gsa_prn = get_prn_data(alldata);
+                break;
+            case 15:  // pdop
+                gsa.pdop = strtod(p, NULL);
+                break;
+            case 16:  // hdop
+                gsa.hdop = strtod(p, NULL);
+                break;
+            case 17:  // vdop
+                end = (char *)malloc(sizeof(p));
+                strncpy(end, p, strlen(p)-3);
+                end[strlen(p)-3] = '\0';
+                gsa.vdop = strtod(end, NULL);
+                free(end);
+            default:
+                break;
+        }
+        p = strsplit(&s, ",");
+        times++;
+    }
+    free(s);
+    return gsa;
+}
+#endif
+
+#if ENABLE_RMC
+// RMC数据解析
+static RMC rmc_data_parse(char *rmc_data)
+{
+    RMC rmc;
+    unsigned char times = 0;
+    char *p;
+    char *s = strdup(rmc_data);
+
+    p = strsplit(&s, ",");
+    while (p)
+    {
+        switch (times)
+        {
+            case 1:   // UTC
+                strcpy(rmc.utc, p);
+                break;
+            case 2:   // pos status
+                rmc.pos_status = p[0];
+                break;
+            case 3:   // lat
+                rmc.lat = strtod(p, NULL);
+                break;
+            case 4:   // lat dir
+                rmc.lat_dir = p[0];
+                break;
+            case 5:   // lon
+                rmc.lon = strtod(p, NULL);
+                break;
+            case 6:   // lon dir
+                rmc.lon_dir = p[0];
+                break;
+            case 7:   // speen Kn
+                rmc.speed_Kn = strtod(p, NULL);
+                break;
+            case 8:   // track true
+                rmc.track_true = strtod(p, NULL);
+                break;
+            case 9:   // date
+                strcpy(rmc.date, p);
+                break;
+            case 10:  // mag var
+                rmc.mag_var = strtod(p, NULL);
+                break;
+            case 11:  // var dir
+                rmc.var_dir = p[0];
+                break;
+            case 14:  // mode ind
+                rmc.mode_ind = p[0];
+                break;
+            default:
+                break;
+        }
+        p = strsplit(&s, ",");
+        times++;
+    }
+    free(s);
+    return rmc;
+}
+#endif
+
+#if ENABLE_VTG
+// VTG数据解析
+static VTG vtg_data_parse(char *vtg_data)
+{
+    VTG vtg;
+    unsigned char times = 0;
+    char *p;
+    char *s = strdup(vtg_data);
+
+    p = strsplit(&s, ",");
+    while (p)
+    {
+        switch (times)
+        {
+            case 1:   // track true
+                vtg.track_true = strtod(p, NULL);
+                break;
+            case 3:   // track mag
+                vtg.track_mag = strtod(p, NULL);
+                break;
+            case 5:   // speed Kn
+                vtg.speed_Kn = strtod(p, NULL);
+                break;
+            case 7:   // speed Km
+                vtg.speed_Km = strtod(p, NULL);
+                break;
+            default:
+                break;
+        }
+        p = strsplit(&s, ",");
+        times++;
+    }
+    free(s);
+    return vtg;
+}
+#endif
+
+#if ENABLE_GSV
+/*
+ * function:  获取GSV字段中的GPS信息
+ * gps_data:  最原始的GPS字符串
+ * stas:      卫星数量
+ * prefix:    GSV信息字段前缀
+*/
+static SAT_INFO *get_sats_info(char *gps_data, unsigned char sats, char *prefix)
+{
+    SAT_INFO *sats_info;
+    unsigned char times = 0;
+    unsigned char msgs = 0;
+    unsigned char msg = 0;
+    unsigned char for_times;
+    unsigned char i;
+    char *p;
+    char *s;
+    char *sentences;
+
+    sats_info = (SAT_INFO *)malloc(sizeof(SAT_INFO) * (sats+1));
+    memset(sats_info, 0, sizeof(SAT_INFO) * (sats+1));
+    sentences = strtok(gps_data, "\r\n");
+    while (sentences)
+    {
+        if (strstr(sentences, prefix))
+        {
+            s = strdup(sentences);
+            p = strsplit(&s, ",");
+            while (p)
+            {
+                switch (times)
+                {
+                    case 1:   // msgs
+                        msgs = (unsigned char) strtol(p, NULL, 10);
+                        break;
+                    case 2:   // msg
+                        msg = (unsigned char) strtol(p, NULL, 10);
+                        break;
+                    case 3:   // sat info
+                        for_times = (msgs == msg) ? ((sats % 4) ? sats % 4 : 4) : 4;
+                        for (i = 0; i < for_times; i++)
+                        {
+                            p = strsplit(&s, ",");
+                            (sats_info+(msg-1)*4+i)->prn = (unsigned char) strtol(p, NULL, 10);
+                            p = strsplit(&s, ",");
+                            (sats_info+(msg-1)*4+i)->elev = (unsigned char) strtol(p, NULL, 10);
+                            p = strsplit(&s, ",");
+                            (sats_info+(msg-1)*4+i)->azimuth = (unsigned short) strtol(p, NULL, 10);
+                            p = strsplit(&s, ",");
+                            (sats_info+(msg-1)*4+i)->SNR = (unsigned char) strtol(p, NULL, 10);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                p = strsplit(&s, ",");
+                times++;
+            }
+            times = 0;
+        }
+        sentences = strtok(NULL, "\r\n");
+    }
+    free(s);
+    return sats_info;
+}
+
+// GSV数据解析
+static GSV gsv_data_parse(char *gsv_data, char *gps_data, char *prefix)
+{
+    GSV gsv;
+    unsigned char times = 0;
+    char *p;
+    char *s = strdup(gsv_data);
+    char *src_data = strdup(gps_data);
+
+    p = strsplit(&s, ",");
+    while (p)
+    {
+        switch (times)
+        {
+            case 1:   // msgs
+                gsv.msgs = (unsigned char)strtol(p, NULL, 10);
+                break;
+            case 2:   // msg
+                gsv.msg = (unsigned char)strtol(p, NULL, 10);
+                break;
+            case 3:   // sats
+                gsv.sats = (unsigned char)strtol(p, NULL, 10);
+                gsv.sat_info = get_sats_info(src_data, gsv.sats, prefix);
+                break;
+            default:
+                break;
+        }
+        p = strsplit(&s, ",");
+        times++;
+    }
+    free(s);
+    return gsv;
+}
+#endif
+
+#if ENABLE_UTC
+// UTC数据解析
+static UTC utc_parse(char *date, char *time)
+{
+    UTC utc_data;
+    unsigned int date_int;
+    double time_float;
+
+    date_int = (unsigned int)strtol(date, NULL, 10);
+    utc_data.DD = date_int / 10000;
+    utc_data.MM = date_int % 10000 / 100;
+    utc_data.YY = date_int % 100;
+    time_float = strtod(time, NULL);
+    utc_data.hh = (unsigned int)time_float / 10000;
+    utc_data.mm = (unsigned int)time_float % 10000 / 100;
+    utc_data.ss = (unsigned int)time_float % 100;
+    utc_data.ds = (unsigned short)(time_float - (unsigned int)time_float);
+
+    return utc_data;
+}
+#endif
+
+// 解析全部的GPS数据
+GPS gps_data_parse(char* gps_src)
+{
+    GPS gps_all;
+    char *str_buffer = strdup(gps_src);
+
+    // GGA数据解析
+#if ENABLE_GGA
+    GGA default_gga_data = {"\0",0.0,'N',0.0,'S',0,0,0,0,0,0,0};
+    gps_src = strdup(str_buffer);
+    gps_all.gga_data = strstr(gps_src, PRE_GGA) ? gga_data_parse(strtok(strstr(gps_src, PRE_GGA), "\r\n")) : default_gga_data;
+#endif
+
+    // GLL数据解析
+#if ENABLE_GLL
+    GLL default_gll_data = {0.0,'\0',0.0,'\0',"\0",'\0'};
+    gps_src = strdup(str_buffer);
+    gps_all.gll_data = strstr(gps_src, PRE_GLL) ? gll_data_parse(strtok(strstr(gps_src, PRE_GLL), "\r\n")) : default_gll_data;
+#endif
+
+    // GSA数据解析
+#if ENABLE_GSA
+    GSA_PRN default_gsa_prn_data = {0,0,0};
+    GSA default_gsa_data = {'\0','\0',0.0,0.0,0.0,&default_gsa_prn_data};
+    gps_src = strdup(str_buffer);
+    gps_all.gsa_data = strstr(gps_src, PRE_GSA) ? gsa_data_parse(strtok(strstr(gps_src, PRE_GSA), "\r\n"), str_buffer) : default_gsa_data;
+#endif
+
+    // RMC数据解析
+#if ENABLE_RMC
+    RMC default_rmc_data = {"\0",'\0',0.0,'\0',0.0,'\0',0.0,0.0,"\0",0.0,'\0','\0'};
+    gps_src = strdup(str_buffer);
+    gps_all.rmc_data = strstr(gps_src, PRE_RMC) ? rmc_data_parse(strtok(strstr(gps_src, PRE_RMC), "\r\n")) : default_rmc_data;
+#endif
+
+    // VTG数据解析
+#if ENABLE_VTG
+    VTG default_vtg_data = {0.0,0.0,0.0,0.0};
+    gps_src = strdup(str_buffer);
+    gps_all.vtg_data = strstr(gps_src, PRE_VTG) ? vtg_data_parse(strtok(strstr(gps_src, PRE_VTG), "\r\n")) : default_vtg_data;
+#endif
+
+    // GSV数据解析
+#if ENABLE_GSV
+    SAT_INFO default_sat_info_data = {0,0,0,0};
+    GSV default_gsv_data = {0,0,0,&default_sat_info_data};
+    // GPGSV数据段解析
+    gps_src = strdup(str_buffer);
+    gps_all.gpgsv_data = strstr(gps_src, PRE_GPGSV) ? gsv_data_parse(strtok(strstr(gps_src, PRE_GPGSV), "\r\n"), str_buffer, PRE_GPGSV) : default_gsv_data;
+    // GNGSV数据段解析
+    gps_src = strdup(str_buffer);
+    gps_all.gngsv_data = strstr(gps_src, PRE_GNGSV) ? gsv_data_parse(strtok(strstr(gps_src, PRE_GNGSV), "\r\n"), str_buffer, PRE_GNGSV) : default_gsv_data;
+    // GLGSV数据段解析
+    gps_src = strdup(str_buffer);
+    gps_all.glgsv_data = strstr(gps_src, PRE_GLGSV) ? gsv_data_parse(strtok(strstr(gps_src, PRE_GLGSV), "\r\n"), str_buffer, PRE_GLGSV) : default_gsv_data;
+#endif
+
+    // UTC数据解析，UTC数据取自RMC段数据
+#if ENABLE_UTC && ENABLE_RMC
+    gps_all.utc = utc_parse(gps_all.rmc_data.date, gps_all.rmc_data.utc);
+#endif
+
+    free(str_buffer);
+    free(gps_src);
+    return gps_all;
+}
---
+++ gps/gps.h
@@ -0,0 +1,182 @@
+//
+// Created by ihz on 2020/6/4.
+//
+
+#ifndef __GPS_H__
+#define __GPS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 根据实际的数据修改ID
+#define PRE_GGA     "$GNGGA"
+#define PRE_GLL     "$GNGLL"
+#define PRE_GSA     "$GNGSA"
+#define PRE_GPGSV   "$GPGSV"
+#define PRE_GNGSV   "$GNGSV"
+#define PRE_GLGSV   "$GLGSV"
+#define PRE_RMC     "$GNRMC"
+#define PRE_VTG     "$GNVTG"
+
+// 语句解析控制 1：使能，0：禁用
+#define ENABLE_GGA  1
+#define ENABLE_GLL  1
+#define ENABLE_GSA  1
+#define ENABLE_GSV  1
+#define ENABLE_RMC  1
+#define ENABLE_VTG  1
+#define ENABLE_UTC  1
+
+#if ENABLE_GGA
+// GGA字段结构体（GPS定位数据）
+typedef struct
+{
+    char utc[11];                     // UTC时间，格式为hhmmss.sss
+    double lat;                       // 纬度，格式为ddmm.mmmm
+    char lat_dir;                     // 纬度半球，N或S
+    double lon;                       // 经度，格式为dddmm.mmmm
+    char lon_dir;                     // 经度半球，E或W
+    unsigned char quality;            // 0=定位无效，1=定位有效
+    unsigned char sats;               // 使用卫星数量，从00到12
+    double hdop;                      // 水平精确度，0.5到99.9，单位m
+    double alt;                       // 海平面的高度，-9999.9到9999.9米
+    double undulation;                // 大地水准面高度，-9999.9到9999.9米
+    unsigned char age;                // 差分时间
+    unsigned short stn_ID;            // 差分站ID号0000 - 1023
+} GGA;
+#endif
+
+#if ENABLE_GLL
+// GPGLL数据结构体（地理定位信息）
+typedef struct
+{
+    double lat;                       // 纬度，格式为ddmm.mmmm
+    char lat_dir;                     // 纬度半球，N或S
+    double lon;                       // 经度，格式为dddmm.mmmm
+    char lon_dir;                     // 经度半球，E或W
+    char utc[11];                     // UTC时间，格式为hhmmss.sss
+    char data_status;                 // 状态标志位，A：有效，V无效
+}GLL;
+#endif
+
+#if ENABLE_GSA
+#pragma pack(1)                       // 便于指针偏移取值
+// 信道信息结构体
+typedef struct
+{
+    unsigned char total;              // 总信道个数
+    unsigned char prn_ID;             // prn信道
+    unsigned char prn;                // PRN 码（伪随机噪声码）
+}GSA_PRN;
+#pragma pack()
+
+//GPGSA数据结构体（当前卫星信息）
+typedef struct
+{
+    unsigned char mode_MA;            // 定位模式(选择2D/3D)，A=自动选择，M=手动选择
+    unsigned char mode_123;           // 定位类型，1=未定位，2=2D定位，3=3D定位
+    double pdop;                      // PDOP综合位置精度因子（0.5 - 99.9）
+    double hdop;                      // HDOP水平精度因子（0.5 - 99.9）
+    double vdop;                      // VDOP垂直精度因子（0.5 - 99.9）
+    GSA_PRN *gsa_prn;                 // 存放信道信息
+}GSA;
+#endif
+
+#if ENABLE_GSV
+#pragma pack(1)                       // 便于指针偏移取值
+// 可见卫星信息结构体
+typedef struct
+{
+    unsigned char prn;                // PRN 码（伪随机噪声码）
+    unsigned char elev;               // 卫星仰角（00 - 90）度
+    unsigned short azimuth;           // 卫星方位角（00 - 359）度
+    unsigned char SNR;                // 信噪比
+}SAT_INFO;
+#pragma pack()
+
+// GPGSV数据结构体(可见卫星信息)
+typedef struct
+{
+    unsigned char msgs;               // 本次GSV语句的总数目（1 - 3）
+    unsigned char msg;                // 本条GSV语句是本次GSV语句的第几条（1 - 3）
+    unsigned char sats;               // 当前可见卫星总数（00 - 12）
+    SAT_INFO *sat_info;               // 卫星信息
+}GSV;
+#endif
+
+#if ENABLE_RMC
+//RMC数据结构体（推荐定位信息数据格式）
+typedef struct
+{
+    char utc[11];                     // UTC时间，hhmmss.sss格式
+    unsigned char pos_status;         // 状态，A=定位，V=未定位
+    double lat;                       // 纬度ddmm.mmmm，度分格式
+    char lat_dir;                     // 纬度N（北纬）或S（南纬）
+    double lon;                       // 经度dddmm.mmmm，度分格式
+    char lon_dir;                     // 经度E（东经）或W（西经）
+    double speed_Kn;                  // 速度
+    double track_true;                // 方位角，度
+    char date[7];                     // UTC日期，DDMMYY格式
+    double mag_var;                   // 磁偏角，（000 - 180）度
+    char var_dir;                     // 磁偏角方向，E=东W=西
+    char mode_ind;                    // 模式，A=自动，D=差分，E=估测，N=数据无效（3.0协议内容）
+}RMC;
+#endif
+
+#if ENABLE_VTG
+//VTG数据结构体（地面速度信息）
+typedef struct
+{
+    double track_true;                // 运动角度，000 - 359，真北参照系
+    double track_mag;                 // 运动角度，000 - 359，磁北参照系
+    double speed_Kn;                  // 水平运动速度（0.00），节，Knots
+    double speed_Km;                  // 水平运动速度（0.00）, 公里/时，km/h
+}VTG;
+#endif
+
+#if ENABLE_UTC
+//UTC时间结构体
+typedef struct
+{
+    unsigned char YY;                 // 年
+    unsigned char DD;                 // 日
+    unsigned char MM;                 // 月
+    unsigned char hh;                 // 时
+    unsigned char mm;                 // 分
+    unsigned char ss;                 // 秒
+    unsigned short ds;                // 毫秒
+}UTC;
+#endif
+
+//定义GPS结构体
+typedef struct
+{
+#if ENABLE_GGA
+    GGA gga_data;
+#endif
+#if ENABLE_GLL
+    GLL gll_data;
+#endif
+#if ENABLE_GSA
+    GSA gsa_data;
+#endif
+#if ENABLE_GSV
+    GSV gpgsv_data;
+    GSV gngsv_data;
+    GSV glgsv_data;
+#endif
+#if ENABLE_RMC
+    RMC rmc_data;
+#endif
+#if ENABLE_VTG
+    VTG vtg_data;
+#endif
+#if ENABLE_UTC
+    UTC utc;
+#endif
+}GPS;
+
+GPS gps_data_parse(char* gps_src);
+
+#endif //__GPS_H__
