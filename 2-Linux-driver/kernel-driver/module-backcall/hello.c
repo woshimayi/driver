eb179e02623a5af42b2457b31fcd491e2919b00c
woshidamayi <woshidamayi@Gmail.com>
2021-02-25 15:42:25 +0800

    问题描述：delete busybox iptables rename uboot
    解决方法：

---
+++ 2-Linux-driver/kernel-driver/module-backcall/hello.c
@@ -0,0 +1,26 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("Dual bsd/GPL");
+
+static int number = 100;
+module_param(number, int, S_IRUGO);
+static int hello_init(void)
+{
+    printk(KERN_ALERT"hello world number is: %d\n", number);
+    return 0;
+}
+module_init(hello_init);
+
+static void hello_exit(void)
+{
+    printk("hello exit");
+}
+module_exit(hello_exit);
+
+void just_a_try(void)
+{
+    printk(KERN_ALERT"sysbol just a try!\n");
+}
+EXPORT_SYMBOL_GPL(just_a_try);
+
---
+++ 2-Linux-driver/kernel-driver/module_2/t2.c
@@ -0,0 +1,9 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+void cleanup_module(void)
+{
+    printk(KERN_ALERT"Hi,GoodBye world\n");
+}
+
+MODULE_LICENSE("GPL v2");
