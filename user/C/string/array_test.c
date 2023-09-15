9eaf7116aedbf0fa5533295e8adc9d8e8455b087
woshidamayi <woshidamayi@Gmail.com>
2023-09-15 11:54:49 +0800

    问题描述: add status array example
    解决方法:

---
+++ user/C/string/array_test.c
@@ -0,0 +1,67 @@
+/*
+ * @*************************************:
+ * @FilePath: /user/C/string/array_test.c
+ * @version:
+ * @Author: dof
+ * @Date: 2023-09-15 11:14:44
+ * @LastEditors: dof
+ * @LastEditTime: 2023-09-15 11:52:39
+ * @Descripttion: 状态机 结构体示例 hash 示例
+ * @**************************************:
+ */
+
+typedef enum
+{
+	BOB_TYPE_25L95,
+	BOB_TYPE_UX3320,
+	BOB_TYPE_I7525,
+	BOB_TYPE_28L95,
+	BOB_TYPE_28L97B,
+	BOB_TYPE_HLC,
+	BOB_TYPE_MINGYI,
+	BOB_TYPE_MAX,
+
+	HG_BOB_INVALID = 0xff //   "INVALID"
+} BobType;
+
+typedef enum
+{
+	PON_TYPE_EPON = 0,
+	PON_TYPE_GPON = 1,
+	PON_TYPE_NONE = 2,
+} PonType;
+
+typedef int (*FuncAddChainRules)(void * handle);
+
+typedef struct array_test
+{
+	char *chain_name;
+	char *table_name;
+	FuncAddChainRules add_chain_rules;
+} array_test;
+
+
+int hg_ipt_flt_input(void * handle)
+{
+	printf("zzzzz %s\n", (char *)handle);
+}
+
+array_test chipId[] = {
+	[BOB_TYPE_25L95]  = {"EPON_25L95", "GPON_25L95_SPT", hg_ipt_flt_input},
+	[BOB_TYPE_UX3320] = {"EPON_3320", "GPON_3320_SPT", hg_ipt_flt_input},
+	[BOB_TYPE_I7525]  = {"EPON_I7525", "GPON_I7525", hg_ipt_flt_input},
+	[BOB_TYPE_28L97B] = {"EPON_28L97B", "GPON_28L97B", hg_ipt_flt_input},
+	[BOB_TYPE_HLC]    = {"EPON_HLC", "GPON_HLC", hg_ipt_flt_input},
+	[BOB_TYPE_MINGYI] = {"EPON_MINGYI", "GPON_MINGYI", hg_ipt_flt_input},
+	[BOB_TYPE_28L95]  = {"EPON_28L95", "GPON_28L95", hg_ipt_flt_input},
+};
+
+int main(int argc, char const *argv[])
+{
+	// for (int i = 0; i < sizeof(chipId) / sizeof(chipId[0]); i++)
+	// {
+	// 	printf("%s %10s\n", chipId[i].chain_name, chipId[i].table_name);
+	// }
+	printf("%s\n", chipId[BOB_TYPE_28L95].chain_name,  chipId[BOB_TYPE_28L95].add_chain_rules(chipId[BOB_TYPE_28L95].chain_name));
+	return 0;
+}
